4f8c17e4a5c2a9cc17f1d7f6324d0b5ba15ceeec
santonssss <sarvarkalmuratov370@gmail.com>
2023-09-18 22:08:31 +0500

    third com

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import React from "react";
-import {
-  Footer,
-  Possibility,
-  Features,
-  Header,
-  WhatGPT3,
-} from "./containers/index.js";
-import { CTA, Brand, Navbar } from "./components";
+import Footer from "./containers/footer/Footer.jsx";
+import Possibility from "./containers/possibility/Possibility.jsx";
+import Features from "./containers/features/Features.jsx";
+import WhatGPT3 from "./containers/WhatGPT3/WhatGPT3.jsx";
+import Header from "./containers/Header/Header.jsx";
+import CTA from "./components/cta/CTA.jsx";
+import Brand from "./components/brand/Brand.jsx";
+import Navbar from "./components/navbar/Navbar.jsx";
 import "./App.css";
 function App() {
   return (
--- src/containers/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Features } from "./features/Features.jsx";
-export { default as Footer } from "./footer/Footer.jsx";
-export { default as Header } from "./header/Header.jsx";
-export { default as Possibility } from "./possibility/Possibility.jsx";
-export { default as WhatGPT3 } from "./whatGPT3/WhatGPT3.jsx";
